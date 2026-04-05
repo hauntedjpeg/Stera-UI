6ed86be2434079391b94a8c5c71cf8a03f492276
Chaz Giese <chazgiese@gmail.com>
2026-04-05 02:09:32 -0400

    style: textarea adjustments

--- registry/ui/textarea.tsx
+++ registry/ui/textarea.tsx
@@ -8,13 +8,13 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
       data-slot="textarea"
       className={cn(
         // Base
-        "flex rounded-lg border border-border bg-transparent px-2.5 py-2 shadow-xs",
+        "flex rounded-xl border border-border bg-transparent px-3 py-2.5 st-body-md",
         // Sizing
         "min-h-16 w-full",
         // Variants
         "md:text-sm",
         // Other
-        "field-sizing-content text-base transition-[color,box-shadow] outline-none placeholder:text-text-secondary focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-ring-danger",
+        "field-sizing-content transition-[color,box-shadow] outline-none placeholder:text-text-secondary focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-ring-danger",
         className,
       )}
       {...props}
