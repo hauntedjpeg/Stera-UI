2aae38db33bee9217d774c812fdc62ee49eb595d
Chaz Giese <chazgiese@gmail.com>
2026-04-10 00:01:33 -0400

    chore: removed stale font stufffff

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -3,7 +3,6 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { ThemeProvider } from "next-themes"
 import "../registry/styles/globals.css"
-import "./fonts.css"
 
 export const metadata: Metadata = {
   title: "Stera UI",
